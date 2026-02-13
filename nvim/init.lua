4708f6f5dc141dee7c3768e0ee4a312a8b69af32
binl233 <835741923@qq.com>
2026-02-13 15:15:01 -0500

    Rearrange the configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,34 +11,14 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('options')
+require('config/options')
 require("lazy").setup({
     spec = {
         { import = "plugins" }
     }
 })
 
-require('keymaps')
-require('lsp')
-require('treesitter-config')
-
-vim.cmd.colorscheme "catppuccin"
-
--- require'nvim-treesitter'.install { 'rust', 'javascript', 'zig', "go", "python", "java", "c", "markdown", "scala" }
--- Force Function Definitions (e.g., func main) to be Blue
-vim.api.nvim_set_hl(0, "@function", { fg = "#61AFEF", bold = true })
-vim.api.nvim_set_hl(0, "@function.builtin", { fg = "#E5C07B" }) -- Built-ins like make, len
-
--- Force Function Calls (e.g., fmt.Println) to be Blue
-vim.api.nvim_set_hl(0, "@function.call", { fg = "#61AFEF" })
-vim.api.nvim_set_hl(0, "@method.call", { fg = "#61AFEF" })
-
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
-
-vim.opt.hlsearch = true
-
+require('config/keymaps')
+require('config/lsp')
+require('config/treesitter-config')
+require('config/colortheme')
