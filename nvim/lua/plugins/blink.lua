15284d69c325d7bbf9df1735a696710ad8a472ab
Kaile Ying <kaile.ying@bytedance.com>
2026-08-03 14:54:09 -0700

    Change keymaps

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -25,7 +25,12 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'enter' },
+    keymap = {
+      preset = 'enter',
+
+      ['<C-j>'] = { 'select_next', 'fallback' },
+      ['<C-k>'] = { 'select_prev', 'fallback' },
+    },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
